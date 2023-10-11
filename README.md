01d62439ec772414c89dac6c39300bf8a74175ae
dvuzu <147317041+dvuzu@users.noreply.github.com>
2023-10-11 12:41:36 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+<hr>
+
+<p align="center"><img src="/images/128.png" alt="hello-roblox logo" height="128"></p>
+
+<h1 align="center">Hello, Roblox!</h1>
+
+<p align="center">Return the old greeting that we all missed! Open Source!</p>
+<p align="center"><img src="/images/home.png" height="128"></p>
+
+<hr>
+
+
+<p align="center">
+  <a href="https://chrome.google.com/webstore/detail/dpceldlkpeeefhaaholbfihfjogjabja"><img src="https://user-images.githubusercontent.com/585534/107280622-91a8ea80-6a26-11eb-8d07-77c548b28665.png" alt="Get Hello Roblox for Chromium"></a>
+  <a href="https://addons.mozilla.org/addon/hello-roblox/"><img src="https://user-images.githubusercontent.com/585534/107280546-7b9b2a00-6a26-11eb-8f9f-f95932f4bfec.png" alt="Get Hello Roblox for Firefox"></a>
+</p>
+
+<hr>
+
+
+## Installation
+You can download in for Firefox <a href="https://addons.mozilla.org/addon/hello-roblox/">here</a> or Chromium version <a href="https://chrome.google.com/webstore/detail/dpceldlkpeeefhaaholbfihfjogjabja">here</a>. You can install it manually too, source code is in the repo.
---
+++ firefox/main.js
@@ -0,0 +1,65 @@
+// Function to wait for an element with the given selector to exist in the DOM
+function waitForElementToExist(selector) {
+    return new Promise(resolve => {
+        // If the element already exists, resolve the promise immediately
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        // Create a MutationObserver to watch for changes in the DOM
+        const observer = new MutationObserver(() => {
+            // Check if the element with the given selector now exists
+            if (document.querySelector(selector)) {
+                // If it exists, resolve the promise and disconnect the observer
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        // Start observing changes in the body of the document
+        observer.observe(document.body, {
+            subtree: true,
+            childList: true,
+        });
+    });
+}
+
+// Immediately invoked async function expression (IIFE)
+(async () => {
+    try {
+        // Find the meta tag with the name "user-data"
+        var userDataMeta = document.querySelector('meta[name="user-data"]');
+
+        // Extract user data attributes or use default values if they don't exist
+        var userId = userDataMeta.getAttribute('data-userid') || '1';
+        var name = userDataMeta.getAttribute('data-name') || 'Name';
+        var displayName = userDataMeta.getAttribute('data-displayname') || 'DisplayName';
+        var isUnder13 = userDataMeta.getAttribute('data-isunder13') === "true";
+        var created = userDataMeta.getAttribute('data-created') || '1/1/2009 1:00:00 AM';
+        var isPremiumUser = userDataMeta.getAttribute('data-ispremiumuser') === "true";
+        var hasVerifiedBadge = userDataMeta.getAttribute('data-hasverifiedbadge') === "true";
+
+        // Wait for the avatar image element to exist
+        var avatarUrl = await waitForElementToExist("#navigation .avatar img");
+
+        // Update the HTML content of a specific element with user data and avatar
+        document.querySelector("#HomeContainer > div.section > div").innerHTML = `
+            <h1 style="display: flex; align-items: center; margin-bottom: 15px;">
+                <a class="avatar avatar-card-fullbody" style="margin-right:15px;width:128px;height:128px;" href="/users/${userId}/profile">
+                    <span class="avatar-card-link friend-avatar icon-placeholder-avatar-headshot" style="width:128px;height:128px;">
+                        <thumbnail-2d class="avatar-card-image">
+                            <span class="thumbnail-2d-container">
+                                <img src="${avatarUrl?.src}" style="background-color: #d4d4d4;"></img>
+                            </span>
+                        </thumbnail-2d>
+                    </span>
+                </a>
+                ${isPremiumUser ? '<span class="icon-premium-medium" style="margin-right: 10px;"></span>' : ""}
+                <a href="/users/${userId}/profile" class="user-name-container">Hello, ${displayName}!</a>
+            </h1>
+        `;
+    } catch (error) {
+        // Log the error to the console
+        console.error(error);
+    }
+})();
