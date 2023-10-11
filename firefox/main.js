670698700a4f51ddd4a51373b056002459e53bfe
dvuzu <147317041+dvuzu@users.noreply.github.com>
2023-10-11 14:37:16 +0200

    Added language support

--- firefox/main.js
+++ firefox/main.js
@@ -24,6 +24,56 @@ function waitForElementToExist(selector) {
     });
 }
 
+// Greetings in different languages
+var greetings = {
+    "id_id": "Halo",
+    "de_de": "Hallo",
+    "en_us": "Hello",
+    "es_es": "Hola",
+    "fr_fr": "Bonjour",
+    "it_it": "Ciao",
+    "pt_br": "Olá",
+    "th_th": "สวัสดี",
+    "zh_cn": "你好",
+    "zh_tw": "你好",
+    "ja_jp": "こんにちは",
+    "ko_kr": "안녕하세요",
+    "ms_my": "Helo",
+    "nb_no": "Hei",
+    "sr_rs": "Здраво",
+    "da_dk": "Hej",
+    "et_ee": "Tere",
+    "fil_ph": "Kamusta",
+    "hr_hr": "Bok",
+    "lv_lv": "Sveiki",
+    "lt_lt": "Sveiki",
+    "hu_hu": "Helló",
+    "nl_nl": "Hallo",
+    "pl_pl": "Cześć",
+    "ro_ro": "Salut",
+    "sq_al": "Përshëndetje",
+    "sl_sl": "Zdravo",
+    "sk_sk": "Ahoj",
+    "fi_fi": "Hei",
+    "sv_se": "Hej",
+    "vi_vn": "Xin chào",
+    "tr_tr": "Merhaba",
+    "uk_ua": "Привіт",
+    "cs_cz": "Ahoj",
+    "el_gr": "Γειά σας",
+    "bs_ba": "Zdravo",
+    "bg_bg": "Здравейте",
+    "ru_ru": "Привет",
+    "kk_kz": "Сәлеметсіздерге",
+    "ar_001": "مرحبًا",
+    "hi_in": "नमस्ते",
+    "bn_bd": "হ্যালো",
+    "si_lk": "හෙලෝ",
+    "my_mm": "မင်္ဂလာ",
+    "ka_ge": "გამარჯობა",
+    "km_kh": "ជំរាប"
+};
+
 // Immediately invoked async function expression (IIFE)
 (async () => {
     try {
@@ -39,6 +89,18 @@ function waitForElementToExist(selector) {
         var isPremiumUser = userDataMeta.getAttribute('data-ispremiumuser') === "true";
         var hasVerifiedBadge = userDataMeta.getAttribute('data-hasverifiedbadge') === "true";
 
+
+        // Find the meta tag with the name "locale-data"
+        var userLocaleDataMeta = document.querySelector('meta[name="locale-data"]');
+
+        // Extract user locale data attributes or use default values if they don't exist
+        var languageCode = userLocaleDataMeta.getAttribute('data-language-code') || 'en_us';
+        var languageName = userLocaleDataMeta.getAttribute('data-language-name') || 'English';
+
+        // Get greeting text from the language code
+        var greeting = greetings[languageCode] || "Hello";
+
+        
         // Wait for the avatar image element to exist
         var avatarUrl = await waitForElementToExist("#navigation .avatar img");
 
@@ -55,7 +117,7 @@ function waitForElementToExist(selector) {
                     </span>
                 </a>
                 ${isPremiumUser ? '<span class="icon-premium-medium" style="margin-right: 10px;"></span>' : ""}
-                <a href="/users/${userId}/profile" class="user-name-container">Hello, ${displayName}!</a>
+                <a href="/users/${userId}/profile" class="user-name-container">${greeting}, ${displayName}!</a>
             </h1>
         `;
     } catch (error) {
